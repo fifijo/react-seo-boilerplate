7f23a4af09ac6de96259264caee538a131252a10
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2018-12-29 12:05:46 +0100

    Navigation/Header components & dummy styled-components example

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
 import styled from 'styled-components'
 import Navigation from 'shared/components/Navigation'
-=======
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
->>>>>>> 5df2276cc3eb921e76753eed74238e145a144c59
 
 const HeaderContainer = styled.div`
   display: flex;
