b13f8b6f0541498f8530cbc5224c3c935df75c53
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2019-03-06 22:30:44 +0100

    Test redux-thunk improvement

--- __tests__/actions/posts.test.js
+++ __tests__/actions/posts.test.js
@@ -3,21 +3,13 @@ import * as actionCreators from 'shared/state/actions/posts'
 import thunk from 'redux-thunk'
 import { 
     GET_POSTS_BEGIN, 
-    GET_POSTS_REQUEST, 
-    GET_POSTS_FAILURE 
+    GET_POSTS_REQUEST
 } from 'shared/state/types'
 
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
 describe('Action posts fetchData', () => {
-    
-    beforeEach(() => {
-        
-    })
-
-    afterEach(() => {
-    })
 
     it('should create begin action', () => {
         expect(actionCreators.begin().type).toEqual(GET_POSTS_BEGIN);
@@ -27,14 +19,18 @@ describe('Action posts fetchData', () => {
         expect(actionCreators.request().type).toEqual(GET_POSTS_REQUEST);
     })
 
-    it('should execute fetch data action then dispatch begin and request', () => {
+    it('should execute fetchData', () => {
         const store = mockStore({})
         const actions = store.getActions()
 
+        const expectedActions = [
+            { type: GET_POSTS_BEGIN },
+            { type: GET_POSTS_REQUEST }
+        ]
+
         return store.dispatch(actionCreators.fetchData())
             .then(() => {
-                expect(actions[0].type).toEqual(GET_POSTS_BEGIN)
-                expect(actions[1].type).toEqual(GET_POSTS_REQUEST)
+                expect(actions).toMatchObject(expectedActions)
             })
     })
   })
